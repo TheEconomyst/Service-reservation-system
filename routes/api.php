ac287c60f5ca4d3a35244280ba37450fafaa79ab
lukas.arlauskas <lukas.arlauskas@iwmac.no>
2022-03-15 21:54:03 +0200

    login backend

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//public routai eina cia
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    //routai kuriem reikia autho eina cia
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/refreshtoken', [\App\Http\Controllers\AuthController::class, 'refresh']);
 });
