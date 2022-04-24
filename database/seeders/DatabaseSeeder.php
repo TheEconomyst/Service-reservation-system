857644071027df21c1696908ae64e12a35a39e37
Ramūnas Mažeikis <r.mazeikis@gmc.lt>
2022-04-24 19:35:50 +0300

    Made a basic seeder.
    Signed-off-by: Ramūnas Mažeikis <r.mazeikis@gmc.lt>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-use App\Models\ReservationForm;
+use App\Models\Company;
+use App\Models\FieldChoice;
+use App\Models\FieldOption;
 use App\Models\FormField;
+use App\Models\ReservationForm;
+use App\Models\Service;
+use App\Models\ServiceProvider;
+use App\Models\User;
+use App\Models\WorkSchedule;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,10 +24,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $companies = Company::factory()
+            ->count(10)
+            ->has(User::factory()
+                ->count(rand(1, 5))
+                ->has(ServiceProvider::factory()
+                    ->count(1)
+                    ->has(WorkSchedule::factory()
+                        ->count(5)
+                    )
+                )
+            )
+            ->has(Service::factory()
+                ->count(5)
+            )
+            ->create()
+            ;
+
         ReservationForm::factory()
             ->count(10)
-            ->has(FormField::factory()->count(3))
+            ->has(FormField::factory()
+                ->count(3)
+            )
             ->create()
             ;
     }
